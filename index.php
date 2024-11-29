3be5fede8846f729070cc1279566171e0e7e5165
kananda07 <kanandaaparecida9@gmail.com>
2024-11-29 16:30:39 -0300

    mousse de maracujá

---
+++ index.php
@@ -0,0 +1,32 @@
+<h2>receita: 
+    <?php
+    echo "mousse de maracuja";
+    ?>
+    </h2>
+        <table border="1">
+            <thead>
+                <tr>
+                    <th>ingredientes</th>
+                    <th>quantidade</th>
+</tr>
+</thead>
+<tbody>
+    <tr>
+        <td> maracujá </td>
+        <td> suco de 2 frutas</td>
+</tr>
+<tr>
+        <td> leite condensado</td>
+        <td>2 caixas</td>
+</tr>
+<tr>
+        <td> creme de leite</td>
+        <td>2 caixas</td>
+</tr>
+
+<h1> nota: </h1>
+<script>
+    var h1=document.querySelector("h1");
+    h1.innerHTML="nota: 9";
+    </script>
+    
